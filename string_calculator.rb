12cb94d9455ef0e23fbecd0890455f74031f4d19
Shah Rukh <shah.xhtml@yahoo.com>
2025-03-20 08:31:36 +0530

    feat: error if negative number

--- string_calculator.rb
+++ string_calculator.rb
@@ -11,7 +11,14 @@ class StringCalculator
     end
 
     def add_multiple(num)
+      validate_negatives(num)
       num.split(delimiter(num)).sum(&:to_i)
+    end
+
+    def validate_negatives(num)
+      return unless num.include?('-')
+
+      raise ArgumentError, 'Negative numbers are not allowed'
     end
   end
 end
