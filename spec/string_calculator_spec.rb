ad7a026192d17db7f7f339dac89fbb0280257dd1
Shah Rukh <shah.xhtml@yahoo.com>
2025-03-20 08:05:43 +0530

    tests: base

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../string_calculator'
+
+RSpec.describe StringCalculator do
+  let(:todo) { skip 'TODO' }
+
+  describe '#add' do
+    it 'returns 0 for an empty string' do
+      todo
+    end
+  end
+end
