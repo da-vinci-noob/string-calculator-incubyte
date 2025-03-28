cf60dea795af6a8578e4d3705b0540b1f94a672a
Shah Rukh <shah.xhtml@yahoo.com>
2025-03-28 08:16:51 +0530

    feat|refacter: Add support for multiplication and improve delimiter regex handling with constants

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,20 +1,30 @@
 class StringCalculator
   class << self
+    # Constants for Statics
+    MAX_NUMBER = 1000
+    DEFAULT_DELIMITER = /,|\n/
+
     def add(input)
       return 0 if input.empty?
 
-      numbers_array = input.split(extract_delimiter(input))
+      delimiter = extract_delimiter(input)
+      numbers_array = split_numbers(input, delimiter)
       validate_negatives(numbers_array)
-      filtered_numbers(numbers_array).sum(&:to_i)
+      calculation(delimiter, numbers_array)
     end
 
     # Extract delimiter from the string between // and \n
     # If no delimiter is specified, using default delimiter
     def extract_delimiter(input)
-      return default_delimiter unless input.start_with?('//')
+      return DEFAULT_DELIMITER unless input.start_with?('//')
 
       delimiter = input.match(%r{//(.+)\n})[1]
-      Regexp.new(Regexp.escape(delimiter).chars.join('|'))
+      return delimiter if delimiter.size == 1
+
+      Regexp.new(
+        delimiter.chars.map { |c| Regexp.escape(c) }
+        .join('|')
+      )
     end
 
     # Default delimiter is comma or new line
@@ -32,7 +42,29 @@ class StringCalculator
 
     # Filter out numbers greater than 1000
     def filtered_numbers(numbers_array)
-      numbers_array.reject { |num| num.to_i > 1000 }
+      numbers_array.reject { |num| num.to_i > MAX_NUMBER }
+    end
+
+    def split_numbers(input, delimiter)
+      return input.split(delimiter) if delimiter == DEFAULT_DELIMITER
+
+      # Find the index of first \n after custom delimiters
+      idx = input.index("\n")
+      # Then Split the rest of the string based on delimiter
+      input[idx + 1..-1].split(delimiter)
+    end
+
+    # Calculate the sum of the numbers
+    def calculation(delimiter, numbers_array)
+      if delimiter == '*'
+        num = 1
+        filtered_numbers(numbers_array).each do |n|
+          num *= n.to_i
+        end
+        num
+      else
+        filtered_numbers(numbers_array).sum(&:to_i)
+      end
     end
   end
 end
